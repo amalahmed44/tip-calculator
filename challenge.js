0a8682ebadb9c89ebb4b7e365742de056c12a0dd
amalali8 <amalza958@gmail.com>
2023-07-15 03:51:56 +0300

    answer

--- challenge.js
+++ challenge.js
@@ -1,2 +1,17 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let bill = 275;
+let tip;
+
+if (bill >= 50 && bill <= 300) {
+  tip = bill * 0.15; 
+} else {
+  tip = bill * 0.2; 
+}
+
+
+let totalValue = bill + tip;
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value is ${totalValue}`
+);
